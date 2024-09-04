f501367b4c2e183ecfa8ed82263c05d1e4b174d2
Moonwalkert3ch <6620@holbertonstudents.com>
2024-09-03 20:24:50 -0500

    added second blog

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ const transporter = nodemailer.createTransport({
 });
 
 // MongoDB connection
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.MONGODB_URI)
   .then(() => console.log('MongoDB connected'))
   .catch(err => console.error('MongoDB connection error:', err));
 
